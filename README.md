5b337f71e6e157ecd7e2ac3aa89c204a7e8d8f23
WiktorLigeza <wiktorligeza@gmail.com>
2025-02-16 14:08:49 +0100

    SETUP

---
+++ README.md
@@ -0,0 +1,2 @@
+An application for viewing and managing services on Linux, providing an intuitive interface to monitor, control, and optimize system processes effortlessly.
+
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "Step 1: Creating virtual environment if it doesn't exist..."
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+echo "Step 2: Activating virtual environment..."
+source venv/bin/activate
+
+echo "Step 3: Installing dependencies..."
+pip install -r requirements.txt
